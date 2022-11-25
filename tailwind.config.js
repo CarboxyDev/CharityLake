1ac513eb107a03ebefe1d8796f3ffeaf919198f3
Arman <37100541+CarboxyDev@users.noreply.github.com>
2022-11-26 04:42:07 +0530

    Setup typescript

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./{pages,components}/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
