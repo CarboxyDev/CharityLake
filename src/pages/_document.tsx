44c5b745084bd0adebb75c4f31c72da51dd31614
Arman <37100541+CarboxyDev@users.noreply.github.com>
2022-11-27 21:23:27 +0530

    Add fonts (Lato and Eudoxus Sans)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,21 @@ const Document = () => {
       <Head>
         <meta name="description" content="The decentralized way to charity" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link rel="preconnect" href="https://stijndv.com" />
+        <link
+          rel="stylesheet"
+          href="https://stijndv.com/fonts/Eudoxus-Sans.css"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
