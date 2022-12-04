1fa15c41b99e08f6493ee3512da305458bb8d790
Arman <37100541+CarboxyDev@users.noreply.github.com>
2022-12-04 22:39:04 +0530

    Update DonateWidget height

--- src/components/Charity/DonateWidget.tsx
+++ src/components/Charity/DonateWidget.tsx
@@ -5,7 +5,7 @@ const DonateWidget = () => {
       <iframe
         src="https://nowpayments.io/embeds/donation-widget?api_key=0GEWDFX-XN14RSF-K845GP2-JX1ZTEV"
         width="354"
-        height="495"
+        height="510"
         frameBorder="0"
         scrolling="no"
         className="overflow-y-hidden "
